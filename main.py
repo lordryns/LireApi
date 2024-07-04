5613a353f895b56564757f448568302f8567bbf2
David <rynstheoverlord@gmail.com>
2024-07-04 16:18:08 +0100

    Added async to routes

--- main.py
+++ main.py
@@ -9,14 +9,9 @@ def trending_books():
     pass
 
 @app.get("/")
-def home():
-    return {"title": "LireApi"}
+async def home():
+    return {"title": "LireApi", "message": "Use /docs to see the documentation."}
 
 @app.get("/search/{title}")
-def search(title: str):
-    title = title.replace(" ", "-")
-    url = f"https://mangareader-api.vercel.app/api/v1/search/{title}"
-    response = requests.get(url)
-
-    result = response.json()["data"]
-    return {"query": title, "response": response.status_code, "result": result}
+async def search(title: str):
+    return {"query": title}
